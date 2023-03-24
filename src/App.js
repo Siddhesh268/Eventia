01c918176263260b8a32fb76e29f05850520823d
Siddhesh268 <shethsiddhesh268@gmail.com>
2023-03-23 23:51:09 -0400

    adding router to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Routes>
+        <Route path="/registration" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
